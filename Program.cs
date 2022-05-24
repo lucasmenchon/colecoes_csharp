adaa6c47ae1a46e7bbf00af82b165ceb5ad3854f
lucasmenchon <it.lucas@outlook.com>
2022-05-24 12:00:47 -0300

    adicionado novos metodos linq

--- Program.cs
+++ Program.cs
@@ -9,18 +9,32 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            // ************* LINQ *************** //
 
-            int[] arrayNum = new int[6] { 1, 3, 5, 6, 8, 10 };
-            var numPar =
-                from num in arrayNum
-                where num % 2 == 0
-                orderby num
-                select num;
 
-            var numParMetodo = arrayNum.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
-            WriteLine("Pares Query:" + string.Join(", ", numPar));
-            WriteLine("Pares Metodo:" + string.Join(", ", numParMetodo));
+            //valor min, med, max | sum, distinct
+            int[] arrayNum = new int[3] { 100, 20, 30 };
+
+            var min = arrayNum.Min();
+            var max = arrayNum.Max();
+            var med = arrayNum.Average();
+            var soma = arrayNum.Sum();
+            var unico = arrayNum.Distinct().ToArray();
+
+            WriteLine($"Min: {min}, Max {max}, Med {med}");
+            WriteLine($"Sum: {string.Join(", ", arrayNum)}");
+            WriteLine($"Distinct: {string.Join(", ", unico)}");
+
+            ////LINQ
+            //int[] arrayNum = new int[6] { 1, 3, 5, 6, 8, 10 };
+            //var numPar =
+            //    from num in arrayNum
+            //    where num % 2 == 0
+            //    orderby num
+            //    select num;
+
+            //var numParMetodo = arrayNum.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            //WriteLine("Pares Query:" + string.Join(", ", numPar));
+            //WriteLine("Pares Metodo:" + string.Join(", ", numParMetodo));
 
 
 
